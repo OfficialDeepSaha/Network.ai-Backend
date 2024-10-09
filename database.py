238bada9065c0b34153cc40fbc5195bb85865885
Deep Saha <115029656+OfficialDeepSaha@users.noreply.github.com>
2024-10-09 14:16:45 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,14 @@
-from sqlalchemy import create_engine 
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.orm import sessionmaker, Session
 from models import Base
-from databases import Database
-import os
 
-# Load the DATABASE_URL from the environment (Render environment variables are already available)
-DATABASE_URL = "postgresql://connector_xu3z_user:bFNP3icqOpvpxchbOaLlexo4M6OAzvY1@dpg-cs2n5et6l47c73blnmt0-a.oregon-postgres.render.com/connector_xu3z"
+# DATABASE_URL = "postgresql://postgres:1234@localhost/connector"
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
+DATABASE_URL= "postgresql://connector_xu3z_user:bFNP3icqOpvpxchbOaLlexo4M6OAzvY1@dpg-cs2n5et6l47c73blnmt0-a.oregon-postgres.render.com/connector_xu3z"
 
-# Sync engine and session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# For async support
-database = Database(DATABASE_URL)
 
 def get_db():
     db = SessionLocal()
